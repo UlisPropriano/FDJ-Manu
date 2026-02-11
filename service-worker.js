13a263f485e2379cfb943413a9ba463686cbdecb
UlisPropriano <mielcorse@gmail.com>
2026-02-11 21:33:37 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+// Service Worker pour mode hors ligne
+const CACHE_NAME = 'fdj-manu-v1';
+const urlsToCache = [
+  './fdj_manu_pwa.html',
+  './manifest.json'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activation du Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Suppression ancien cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interception des requêtes réseau
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retourne la ressource du cache si disponible
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
